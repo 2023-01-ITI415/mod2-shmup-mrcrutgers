b2ad420cbc59af23ef197cc40c8f0c824c5c2700
mrcrutgers <123099825+mrcrutgers@users.noreply.github.com>
2023-02-23 11:55:46 -0500

    Edited bounds check, enemy

--- Assets/Assets by Peter Burroughs/__Scripts/BoundsCheck.cs
+++ Assets/Assets by Peter Burroughs/__Scripts/BoundsCheck.cs
@@ -5,14 +5,28 @@ using UnityEngine;
 public class BoundsCheck : MonoBehaviour
 {
 
+[System.Flags] 
+public enum eScreenLocs { // a 
+onScreen = 0, // 0000 in binary (zero) 
+offRight = 1, // 0001 in binary 
+offLeft = 2, // 0010 in binary 
+offUp = 4, // 0100 in binary 
+offDown = 8 // 1000 in binary 
+}
+
+
+
 public enum eType { center, inset, outset };
 [Header("Inscribed")] 
 public eType boundsType = eType.center; // a 
 public float radius = 1f;
+public bool keepOnScreen = true;
 
 
 
 [Header("Dynamic")] 
+public eScreenLocs screenLocs = eScreenLocs.onScreen;
+public bool isOnScreen = true;
 public float camWidth; 
 public float camHeight; 
 
@@ -32,35 +46,54 @@ void LateUpdate () {
 
 
     Vector3 pos = transform.position; 
+    isOnScreen = true;
 
     // Restrict the X position to camWidth 
     if (pos.x > camWidth + checkRadius) {
 
-        pos.x = camWidth + checkRadius; // e 
+        pos.x = camWidth + checkRadius;
+        
+        isOnScreen = false; 
+        // e 
 
         } 
 
     if (pos.x < -camWidth - checkRadius) { // e 
 
-        pos.x = -camWidth - checkRadius; // e 
+        pos.x = -camWidth - checkRadius;
+        
+        isOnScreen = false; 
+        // e 
 
         }
 
     // Restrict the Y position to camHeight 
     if (pos.y > camHeight + checkRadius) { // e 
 
-        pos.y = camHeight + checkRadius; // e 
+        pos.y = camHeight + checkRadius;
+        
+        isOnScreen = false; // e 
 
         } 
 
     if (pos.y < -camHeight -checkRadius) { // e 
 
-        pos.y = -camHeight - checkRadius; // e 
+        pos.y = -camHeight - checkRadius;
+        
+        isOnScreen = false; // e 
 
         } 
 
-    transform.position = pos; 
-}
+    if ( keepOnScreen && !isOnScreen ) {
+            transform.position = pos; 
+            isOnScreen = true;
+            }
+
+    }
+
+
+
+    
 
 
 
---
+++ Assets/Assets by Peter Burroughs/__Scripts/Enemy.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent( typeof( BoundsCheck) )]
+
+public class Enemy : MonoBehaviour
+{
+  
+
+[Header("Inscribed")] 
+public float speed = 10f; 
+// The movement speed is 10m/ s 
+public float fireRate = 0.3f; // Seconds/ shot (Unused) 
+public float health = 10; // Damage needed to destroy this enemy 
+public int score = 100; // Points earned for destroying this 
+
+private BoundsCheck bndCheck;
+
+
+void Awake() { // c 
+
+bndCheck = GetComponent <BoundsCheck>(); 
+
+}
+
+// This is a Property: A method that acts like a field
+public Vector3 pos { // a 
+
+get { 
+    return this.transform.position; 
+    } 
+
+set { 
+    this.transform.position = value; 
+    } 
+}
+
+
+
+
+    // Update is called once per frame
+void Update()
+    {
+     Move();
+
+     // Check whether this Enemy has gone off the bottom of the screen 
+     if ( !bndCheck.isOnScreen ) { // d 
+        if ( pos.y < bndCheck.camHeight - bndCheck.radius ) { 
+            // We’re off the bottom, so destroy this GameObject 
+            Destroy( gameObject ); 
+            } 
+        }
+
+     //b
+    }
+
+
+public virtual void Move() { // c 
+    Vector3 tempPos = pos; 
+    tempPos.y -= speed * Time.deltaTime; 
+    pos = tempPos; 
+}
+
+
+
+
+}
